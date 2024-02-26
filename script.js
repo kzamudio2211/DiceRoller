2cf3ef3c4ca546fbe9b7d19192758a22855da0d9
Kevin <kzamudio2211@gmail.com>
2024-02-25 22:06:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 function getRandomNumber() {
   fetch('https://serverdice.azurewebsites.net/get_random_number')
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Network response was not ok: ${response.status}`);
-          }
-          return response.json();
-      })
-      .then(data => {
-          document.getElementById('randomNumber').innerHTML = `Random Number: ${data.random_number}`;
-      })
-      .catch(error => {
-          console.error('Error fetching random number:', error);
-      });
+      document.getElementById('randomNumber').innerHTML = `You rolled a: ${data.random_number}`;
 }
