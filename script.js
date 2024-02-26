b3494abc443c4faad37cb044abff27d89eea7d3b
Kevin <kzamudio2211@gmail.com>
2024-02-25 21:51:49 -0600

    test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getRandomNumber() {
   try {
       const response = await fetch('https://serverdice.azurewebsites.net/get_random_number');
       const data = await response.json();
-      document.getElementById('randomNumber').innerHTML = `You rolled a: ${data.random_number}`;
+      document.getElementById('randomNumber').innerHTML = `Random Number: ${data.random_number}`;
   } catch (error) {
       console.error('Error fetching random number:', error);
   }
